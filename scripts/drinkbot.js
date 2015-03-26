fae17ffa38071e3cadefeba8207e1f86b0406974
jdrydn <james@jdrydn.com>
2015-03-26 21:47:31 +0000

    Trying to get these damn scripts to work.

--- scripts/drinkbot.js
+++ scripts/drinkbot.js
@@ -6,7 +6,37 @@ drink.needADrink = function needADrink(type) {
   };
 };
 
+var commands = {
+  tea: {
+    'i need a tea': 'needADrink'
+  },
+  beer: {
+    'i need a beer': 'needADrink'
+  }
+};
+
 module.exports = function drinkBot(robot) {
-  robot.respond(/need a tea/i, drink.needADrink('tea'));
-  robot.respond(/need a beer/i, drink.needADrink('beer'));
+  /**
+   * For each type of command.
+   */
+  for (var type in commands) {
+    if (commands.hasOwnProperty(type)) {
+      /**
+       * For each command
+       */
+      for (command in commands[type]) {
+        if (commands[type].hasOwnProperty(command)) {
+          /**
+           * If that command references a valid function.
+           */
+          if (drink.hasOwnProperty(commands[type][command]) && (typeof drink[commands[type][command]] === "function")) {
+            robot.respond(new RegExp(command, 'i'), drink[commands[type][command]]([type]));
+          }
+          else {
+            console.error("Invalid drink command:", commands[type][command]);
+          }
+        }
+      }
+    }
+  }
 };
