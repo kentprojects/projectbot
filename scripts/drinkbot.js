80f8d60fc70964d5453be27f86c0aeef6df90ccc
jdrydn <james@jdrydn.com>
2015-03-26 21:55:55 +0000

    Hehe.

--- scripts/drinkbot.js
+++ scripts/drinkbot.js
@@ -1,42 +1,41 @@
 var drink = {};
 
-drink.needADrink = function needADrink(type) {
+drink.needADrink = function iNeedADrink(type) {
   return function needADrink(message) {
     message.reply('Perhaps it\'s time to get a ' + type + ' round underway?');
   };
 };
 
 var commands = {
-  tea: {
-    'i need a tea': 'needADrink'
-  },
-  beer: {
-    'i need a beer': 'needADrink'
-  }
+  'i need a DRINK': 'iNeedADrink',
+  '([a-z ,.\'-]+) needs a DRINK': 'theyNeedADrink'
 };
 
+var types = ['tea', 'beer'];
+
 module.exports = function drinkBot(robot) {
-  /**
-   * For each type of command.
-   */
-  for (var type in commands) {
-    if (commands.hasOwnProperty(type)) {
-      /**
-       * For each command
-       */
-      for (command in commands[type]) {
-        if (commands[type].hasOwnProperty(command)) {
+  types.forEach(function (type) {
+    /**
+     * For each command
+     */
+    for (var command in commands) {
+      if (commands.hasOwnProperty(command)) {
+        /**
+         * If that command references a valid function.
+         */
+        if (drink.hasOwnProperty(commands[command]) && (typeof drink[commands[command]] === "function")) {
           /**
-           * If that command references a valid function.
+           * Super subtly replace DRINK with the current type.
            */
-          if (drink.hasOwnProperty(commands[type][command]) && (typeof drink[commands[type][command]] === "function")) {
-            robot.respond(new RegExp(command, 'i'), drink[commands[type][command]]([type]));
-          }
-          else {
-            console.error("Invalid drink command:", commands[type][command]);
-          }
+          robot.respond(
+            new RegExp(command.replace('DRINK', type), 'i'),
+            drink[commands[command]]([type])
+          );
+        }
+        else {
+          console.error("Invalid drink command:", commands[command]);
         }
       }
     }
-  }
+  });
 };
