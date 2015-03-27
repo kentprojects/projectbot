4c7a9dded1f86972f06435d905277e3df21ab4fd
jdrydn <james@jdrydn.com>
2015-03-27 10:45:32 +0000

    Tabs to spaces.

--- scripts/drinkbot.js
+++ scripts/drinkbot.js
@@ -2,67 +2,70 @@ var drink = {};
 var slack = require('../slack');
 
 drink.iNeedADrink = function iNeedADrink(type) {
-  return function iNeedADrink(message) {
-    message.reply('Perhaps it\'s time to get a ' + type + ' round underway?');
-  };
+	return function iNeedADrink(message) {
+		message.reply('Perhaps it\'s time to get a ' + type + ' round underway?');
+	};
 };
 
 drink.theyNeedADrink = function theyNeedADrink(type) {
-  return function theyNeedADrink(message) {
-    message.reply('Be a lamb and get ' + message.match[1] + ' a ' + type + '?');
-  };
+	return function theyNeedADrink(message) {
+		message.reply('Be a lamb and get ' + message.match[1] + ' a ' + type + '?');
+	};
 };
 
 drink.drinkTime = function drinkTime(type) {
-  return function drinkTime(message) {
-    if (!message.envelope || !message.envelope.room) {
-      message.reply('I don\'t know which room we\'re in!');
-      return;
-    }
-
-    console.log(message.envelope);
-
-    if (/* false && */ message.envelope.user && message.envelope.user.name && message.envelope.user.room &&
-    (message.envelope.user.name === message.envelope.user.room)) {
-      message.reply('Perhaps you could make it yourself this time?');
-      return;
-    }
-
-    message.reply('Be a lamb and get ' + message.match[1] + ' a ' + type + '?');
-  };
+	return function drinkTime(message) {
+		if (!message.envelope || !message.envelope.room) {
+			message.reply('I don\'t know which room we\'re in!');
+			return;
+		}
+		
+		console.log(message.envelope);
+		
+		if (
+			// false &&
+			message.envelope.user && message.envelope.user.name && message.envelope.user.room &&
+			(message.envelope.user.name === message.envelope.user.room)
+		) {
+			message.reply('Perhaps you could make it yourself this time?');
+			return;
+		}
+		
+		message.reply('Be a lamb and get ' + message.match[1] + ' a ' + type + '?');
+	};
 };
 
 var commands = {
-  'i need a DRINK': 'iNeedADrink',
-  '([a-z ,.\'-]+) needs a DRINK': 'theyNeedADrink',
-  'DRINK time': 'drinkTime'
+	'i need a DRINK': 'iNeedADrink',
+	'([a-z ,.\'-]+) needs a DRINK': 'theyNeedADrink',
+	'DRINK time': 'drinkTime'
 };
 
 var types = ['tea', 'beer'];
 
 module.exports = function drinkBot(robot) {
-  types.forEach(function (type) {
-    /**
-     * For each command
-     */
-    for (var command in commands) {
-      if (commands.hasOwnProperty(command)) {
-        /**
-         * If that command references a valid function.
-         */
-        if (drink.hasOwnProperty(commands[command]) && (typeof drink[commands[command]] === "function")) {
-          /**
-           * Super subtly replace DRINK with the current type.
-           */
-          robot.respond(
-            new RegExp(command.replace('DRINK', type), 'i'),
-            drink[commands[command]]([type])
-          );
-        }
-        else {
-          console.error("Invalid drink command:", commands[command]);
-        }
-      }
-    }
-  });
+	types.forEach(function (type) {
+		/**
+		* For each command
+		*/
+		for (var command in commands) {
+			if (commands.hasOwnProperty(command)) {
+				/**
+				* If that command references a valid function.
+				*/
+				if (drink.hasOwnProperty(commands[command]) && (typeof drink[commands[command]] === "function")) {
+					/**
+					* Super subtly replace DRINK with the current type.
+					*/
+					robot.respond(
+						new RegExp(command.replace('DRINK', type), 'i'),
+						drink[commands[command]]([type])
+					);
+				}
+				else {
+					console.error("Invalid drink command:", commands[command]);
+				}
+			}
+		}
+	});
 };
--- slack.js
+++ slack.js
@@ -6,40 +6,40 @@ var slack = {};
 module.exports = slack;
 
 if (!process.env.HUBOT_SLACK_TOKEN) {
-  console.error("Missing HUBOT_SLACK_TOKEN environment variable.");
-  module.exports = {};
+	console.error("Missing HUBOT_SLACK_TOKEN environment variable.");
+	module.exports = {};
 }
 
 function slackRequest(url, opts, callback) {
-  url = 'https://slack.com/api/' + url + '?token=' + process.env.HUBOT_SLACK_TOKEN;
-  for (var opt in opts) {
-    if (opts.hasOwnProperty(opt)) {
-      url += '&' + opt + "=" + opts[opt];
-    }
-  }
-  debug(url);
-
-  https.get(url, function onSlackResponse(response) {
-    var data = '';
-    response.setEncoding('utf-8');
-    response.on('data', function (chunk) {
-      data += chunk;
-    });
-
-    response.on('end', function () {
-      try {
-        data = JSON.parse(data);
-        debug(data);
-        callback(null, data);
-      }
-      catch (error) {
-        callback(error);
-      }
-    });
-  }).on('error', function (error) {
-    console.error(error);
-    callback(error);
-  });
+	url = 'https://slack.com/api/' + url + '?token=' + process.env.HUBOT_SLACK_TOKEN;
+	for (var opt in opts) {
+		if (opts.hasOwnProperty(opt)) {
+			url += '&' + opt + "=" + opts[opt];
+		}
+	}
+	debug(url);
+	
+	https.get(url, function onSlackResponse(response) {
+		var data = '';
+		response.setEncoding('utf-8');
+		response.on('data', function (chunk) {
+			data += chunk;
+		});
+		
+		response.on('end', function () {
+			try {
+				data = JSON.parse(data);
+				debug(data);
+				callback(null, data);
+			}
+			catch (error) {
+				callback(error);
+			}
+		});
+	}).on('error', function (error) {
+		console.error(error);
+		callback(error);
+	});
 }
 
 /**
@@ -49,59 +49,59 @@ function slackRequest(url, opts, callback) {
  * @return void
  */
 slack.listUsers = function listSlackUsers(callback) {
-  slackRequest('users.list', {}, function (error, data) {
-    if (error) {
-      callback(error);
-    }
-    else if (!data.members) {
-      callback(new Error('No members array returned from Slack.'));
-    }
-    else {
-      callback(null, data.members.filter(function removeDeletedUsers(user) {
-        return !user.is_deleted && !user.is_bot;
-      }).map(function formatUsers(user) {
-        return {
-          id: user.id,
-          name: user.profile.real_name,
-          first_name: user.profile.first_name,
-          email: user.profile.email
-        };
-      }));
-    }
-  });
+	slackRequest('users.list', {}, function (error, data) {
+		if (error) {
+			callback(error);
+		}
+		else if (!data.members) {
+			callback(new Error('No members array returned from Slack.'));
+		}
+		else {
+			callback(null, data.members.filter(function removeDeletedUsers(user) {
+				return !user.is_deleted && !user.is_bot;
+			}).map(function formatUsers(user) {
+				return {
+					id: user.id,
+					name: user.profile.real_name,
+					first_name: user.profile.first_name,
+					email: user.profile.email
+				};
+			}));
+		}
+	});
 };
 
 slack.listUsersAndPresence = function listSlackUsersAndPresence(callback) {
-  slack.listUsers(function onUsersListed(error, users) {
-    if (error) {
-      callback(error);
-    }
-    else {
-      slack.getPresenceForTeam(users, callback);
-    }
-  });
+	slack.listUsers(function onUsersListed(error, users) {
+		if (error) {
+			callback(error);
+		}
+		else {
+			slack.getPresenceForTeam(users, callback);
+		}
+	});
 };
 
 slack.getPresenceForTeam = function getPresenceForTeam(users, callback) {
-  async.map(
-    users,
-    function (user, done) {
-      slackRequest('users.getPresence', {user: user.id}, function (error, result) {
-        if (error) {
-          done(error);
-        }
-        else {
-          user.presence = result.presence;
-          done(null, user);
-        }
-      });
-    },
-    callback
-  );
+	async.map(
+		users,
+		function (user, done) {
+			slackRequest('users.getPresence', {user: user.id}, function (error, result) {
+				if (error) {
+					done(error);
+				}
+				else {
+					user.presence = result.presence;
+					done(null, user);
+				}
+			});
+		},
+		callback
+	);
 };
 
 if (!module.parent) {
-  slack.listUsersAndPresence(function(error, users) {
-    console.log(error, users);
-  });
+	slack.listUsersAndPresence(function(error, users) {
+		console.log(error, users);
+	});
 }
